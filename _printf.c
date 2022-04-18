f145cfa0807b6fcd2cc13d23123c4dfcd9f08832
cballa10 <99319960+cballa10@users.noreply.github.com>
2022-04-18 16:14:35 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
-   (void) format;
-   return printf("Test\n");
+    (void) format;
+    return printf("Test\n");
 }
